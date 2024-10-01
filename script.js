dc332b02eb1e88c59f06d619bee7bb5ba33e947c
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-01 20:17:15 +0500

    6-question end

---
+++ script.js
@@ -0,0 +1,72 @@
+// // 1-SAVOL JAVOBI
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// // 2-SAVOL JAVOBI
+
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i);
+// }
+
+// // 3-SAVOL JAVOBI
+
+// let num = +prompt("Istalgan bir son kiriting");
+
+// for (let i = 1; i <= num * num; i++) {
+//     console.log(i);
+// }
+
+// // 4-SAVOL JAVOBI
+// for (let i = 0; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// // 5-SAVOL JAVOBI
+// for (let i = 1; i <= 15; i += 2) {
+//   console.log(i);
+// }
+
+// // 6-SAVOL JAVOBI
+// for (let i = 1; i <= 5; i++) {
+//     let num = prompt(`Iltimos, ${i}-sonni kiriting:`);
+//     console.log(num);
+// }
+
+// // 7-SAVOL JAVOBI
+
+// // 8-SAVOL JAVOBI
+
+// // 9-SAVOL JAVOBI
+
+// // 10-SAVOL JAVOBI
+
+// // 11-SAVOL JAVOBI
+
+// // 12-SAVOL JAVOBI
+
+// // 13-SAVOL JAVOBI
+
+// // 14-SAVOL JAVOBI
+
+// // 15-SAVOL JAVOBI
+
+// // 16-SAVOL JAVOBI
+
+// // 17-SAVOL JAVOBI
+
+// // 18-SAVOL JAVOBI
+
+// // 19-SAVOL JAVOBI
+
+// // 20-SAVOL JAVOBI
+
+// // 21-SAVOL JAVOBI
+
+// // 22-SAVOL JAVOBI
+
+// // 23-SAVOL JAVOBI
+
+// // 24-SAVOL JAVOBI
+
+// // 25-SAVOL JAVOBI

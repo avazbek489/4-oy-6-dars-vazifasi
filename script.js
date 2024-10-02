2e6361968678da1be77d07d9b178148370f6e5ae
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-02 20:50:28 +0500

    15-question end

--- script.js
+++ script.js
@@ -34,23 +34,57 @@
 // }
 
 // // 7-SAVOL JAVOBI
-
+// for(let i = 15; i >= 5; i--){
+//     console.log(i);
+// }
 // // 8-SAVOL JAVOBI
+// let num = +prompt("Istalgan bir son kiriting")
+// for(let i = 1; i <= num; i++){
+//     console.log(i);
+// }
 
 // // 9-SAVOL JAVOBI
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//     let num = +prompt(`${i}-Sonni kriting`)
+//     sum = num + sum
+// }
+// alert(sum);
 
 // // 10-SAVOL JAVOBI
-
+// for(let i = 1; i <= 50; i++){
+//     if(i % 5 == 0){
+//         console.log(i);
+//     }
+// }
 // // 11-SAVOL JAVOBI
-
+// for (let i = 100; i >= 1; i--) {
+//   console.log(i);
+// }
 // // 12-SAVOL JAVOBI
-
+// let sum = 0;
+// for (let i = 1; i <= 100; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
 // // 13-SAVOL JAVOBI
-
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// for (let i = num1; i <= num2; i++) {
+//   console.log(i);
+// }
 // // 14-SAVOL JAVOBI
-
+// let num = +prompt("Istalgan son kiriting")
+// for (let i = 1; i <= num; i++) {
+//     if (num % i == 0) {
+//         console.log(i);
+//     }
+// }
 // // 15-SAVOL JAVOBI
-
+// for(let i = 1; i <= 20; i++){
+//     console.log(i**2);
+// }
 // // 16-SAVOL JAVOBI
 
 // // 17-SAVOL JAVOBI
